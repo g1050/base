808e271576cef2a337aa9153dcb95adffea532dd
sky <1005035266@qq.com>
2024-05-27 11:17:22 +0000

    fix: remove

--- script/start_conda.sh
+++
@@ -1,1 +0,0 @@
-conda create -n python3.8 python=3.8
--- ssd/multi_obj.py
+++
@@ -1,7 +0,0 @@
-%matplotlib inline
-import torch
-from d2l import torch as d2l
-
-img = d2l.plt.imread('../img/catdog.jpg')
-h, w = img.shape[:2]
-h, w
