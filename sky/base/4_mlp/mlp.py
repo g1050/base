e0fd49ca37cf358f7be00b7bba972c62f844daa4
sky <1005035266@qq.com>
2024-06-07 07:08:42 +0000

    feat: weight decay

--- sky/base/4_mlp/mlp.py
+++ sky/base/4_mlp/mlp.py
@@ -3,8 +3,101 @@
 # 单层感知机等价于批量为1的梯度下降
 # 激活函数：非线性，sigmoid(0,1)、tanh(-1,1)、ReLU
 # mlp vs svm
+import torch
+from torch import nn
+from d2l import torch as d2l
+import torchvision
+from torch.utils import data
+from torchvision import transforms
+
 def mlp_zero():
     pass
 
+def mlp_torch():
+    class MLP(nn.Module):
+        def __init__(self):
+            super().__init__()
+            self.model1 = nn.Sequential(
+                nn.Flatten(),
+                nn.Linear(784, 256),
+                nn.ReLU(),
+                nn.Linear(256, 10)
+            )
+        def forward(self,x):
+            return self.model1(x)
+    def init_weights(m):
+        if type(m) == nn.Linear:
+            nn.init.normal_(m.weight, std=0.01)
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    input = torch.randn(28,28) # 标准正态分布,flatten期望(n,c,h,w)
+    input = torch.randn(1,1,28,28)
+    print(input.shape)
+    model = MLP()
+    model.apply(init_weights)
+    print(model)
+    model = model.to(device)
+    input = input.to(device)
+    output = model(input)
+    print(output.shape)
+
+    batch_size, lr, num_epochs = 256,0.1,10
+    loss = nn.CrossEntropyLoss(reduction='none')
+    ## 
+    # 'none'：不进行任何归约（即不对批次中的损失求和或求平均），返回每个样本的损失。
+    # 'mean'：对批次中的损失求平均，返回一个标量。
+    # 'sum'：对批次中的损失求和，返回一个标量。
+    loss = loss.to(device)
+    optimizer = torch.optim.SGD(model.parameters(),lr=lr)
+
+    trans = transforms.ToTensor()
+    mnist_train = torchvision.datasets.FashionMNIST(root="../../download",train=True,transform=trans,download=True)
+    mnist_test = torchvision.datasets.FashionMNIST(root="../../download",train=False,transform=trans,download=True)
+    
+    train_iter = data.DataLoader(mnist_train,batch_size,shuffle=True,num_workers=4)
+    test_iter = data.DataLoader(mnist_test,batch_size,shuffle=False,num_workers=4)
+
+    # timer = d2l.Timer()
+    # for x,y in train_iter:
+    #     # continue
+    #     print(f"x {x.shape} y {y[0]}") # x的shape N,C,H,W
+    # print(f"length of train dataset {len(mnist_train)}")
+    # print(f"length of train {len(train_iter)},time {timer.stop()}sec")
+
+    # 开始训练
+
+    def train_epoch(net, train_iter, loss, optimizer, device):
+        net.train()
+        for X, y in train_iter:
+            X, y = X.to(device), y.to(device)
+            optimizer.zero_grad()
+            y_hat = net(X)
+            l = loss(y_hat, y).mean()
+            l.backward()
+            optimizer.step()
+    
+    def evaluate_accuracy(net, data_iter, device):
+        net.eval()
+        correct, total = 0, 0
+        with torch.no_grad():
+            for X, y in data_iter:
+                X, y = X.to(device), y.to(device)
+                y_hat = net(X)
+                # print("-"*20)
+                # print(X.shape,y.shape)
+                # print(y_hat.shape)
+                # print("-"*20)
+                predicted = torch.argmax(y_hat, dim=1)
+                total += y.size(0)
+                correct += (predicted == y).sum().item()
+        return correct / total
+
+    for epoch in range(num_epochs):
+        train_epoch(model, train_iter, loss, optimizer, device)
+        train_acc = evaluate_accuracy(model, train_iter, device)
+        test_acc = evaluate_accuracy(model, test_iter, device)
+        print(f'Epoch {epoch + 1}, Train acc {train_acc:.4f}, Test acc {test_acc:.4f}')
+
 if __name__ == "__main__":
-    mlp_zero()
+    # mlp_zero()
+    mlp_torch()
--- sky/base/4_mlp/underfit_overfit.py
+++ sky/base/4_mlp/underfit_overfit.py
@@ -13,3 +13,91 @@
 
 # K-折交叉验证
 # 数据集不够的时候采用，但是计算成本高，深度学习一般不使用
+
+# 使用多项式拟合来变现，过拟合的表现就是train loss下降,test loss确不下降/上升
+
+import math
+import numpy as np
+import torch
+from torch import nn
+from d2l import torch as d2l
+from torch.utils.tensorboard import SummaryWriter
+writer = SummaryWriter()
+
+def overfit():
+    max_degree = 20  # 多项式的最大阶数
+    n_train, n_test = 100, 100  # 训练和测试数据集大小
+    true_w = np.zeros(max_degree)  # 分配大量的空间
+    true_w[0:4] = np.array([5, 1.2, -3.4, 5.6]) # 存放多项式的系数
+
+    # 生成特征数据
+    features = np.random.normal(size=(n_train + n_test, 1)) # 特征只有一维
+    np.random.shuffle(features)
+    # 升成多项式数据
+    poly_features = np.power(features, np.arange(max_degree).reshape(1, -1)) # 计算各个幂次[0,20)幂次
+    for i in range(max_degree):
+        poly_features[:, i] /= math.gamma(i + 1)  # gamma(n)=(n-1)! 对每列分别计算阶乘
+    
+    # labels的维度:(n_train+n_test,)
+    labels = np.dot(poly_features, true_w) # x乘系数
+    labels += np.random.normal(scale=0.1, size=labels.shape) # 加入噪声
+
+    # NumPy ndarray转换为tensor
+    true_w, features, poly_features, labels = [torch.tensor(x, dtype=
+        torch.float32) for x in [true_w, features, poly_features, labels]]
+
+    print(features.shape,poly_features.shape,labels.shape)
+    print(true_w)
+    # features[:2], poly_features[:2, :], labels[:2]
+    def evaluate_loss(net, data_iter, loss):  #@save
+        """评估给定数据集上模型的损失"""
+        metric = d2l.Accumulator(2)  # 损失的总和,样本数量
+        for X, y in data_iter:
+            out = net(X)
+            y = y.reshape(out.shape)
+            l = loss(out, y)
+            metric.add(l.sum(), l.numel())
+        return metric[0] / metric[1]
+
+    def train(train_features, test_features, train_labels, test_labels,tag_name:str,
+          num_epochs=400):
+        loss = nn.MSELoss(reduction='none')
+        input_shape = train_features.shape[-1]
+        # 不设置偏置，因为我们已经在多项式中实现了它
+        net = nn.Sequential(nn.Linear(input_shape, 1, bias=False))
+        batch_size = min(10, train_labels.shape[0])
+        train_iter = d2l.load_array((train_features, train_labels.reshape(-1,1)),
+                                    batch_size)
+        test_iter = d2l.load_array((test_features, test_labels.reshape(-1,1)),
+                                batch_size, is_train=False)
+        trainer = torch.optim.SGD(net.parameters(), lr=0.01)
+        animator = d2l.Animator(xlabel='epoch', ylabel='loss', yscale='log',
+                                xlim=[1, num_epochs], ylim=[1e-3, 1e2],
+                                legend=['train', 'test'])
+        for epoch in range(num_epochs):
+            d2l.train_epoch_ch3(net, train_iter, loss, trainer)
+            if epoch == 0 or (epoch + 1) % 20 == 0:
+                # animator.add(epoch + 1, (evaluate_loss(net, train_iter, loss),
+                #                         evaluate_loss(net, test_iter, loss)))
+                train_loss = evaluate_loss(net, train_iter, loss)
+                test_loss = evaluate_loss(net, test_iter, loss)
+                writer.add_scalars(tag_name,{"test_loss":test_loss,"train_loss":train_loss},epoch)
+                print(train_loss,test_loss)
+        print('weight:', net[0].weight.data.numpy())
+
+    # 正常
+    train(poly_features[:n_train, :], poly_features[n_train:, :],
+        labels[:n_train], labels[n_train:],"normal")
+
+    # 欠拟合
+    # 从多项式特征中选择前2个维度，即1和x
+    train(poly_features[:n_train, :2], poly_features[n_train:, :2],
+        labels[:n_train], labels[n_train:],"underfit")
+    
+    # 过拟合
+    # 从多项式特征中选取所有维度
+    # 由于模型不复杂，所以过拟合的的预测结果也差不多
+    train(poly_features[:n_train, :], poly_features[n_train:, :],
+        labels[:n_train], labels[n_train:],"overfit",num_epochs=1500)
+if __name__ == "__main__":
+    overfit()
